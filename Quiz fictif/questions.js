45edbbe6f20e4fe80368ffb529f5ad77e9d6efa6
Blue-974 <157591945+Blue-974@users.noreply.github.com>
2024-03-20 23:14:29 +0100

    Add files via upload

---
+++ Quiz fictif/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Quel est le plus grand océan du monde ?",
+        answer: "C. Océan Pacifique",
+        options: [
+            "A. Océan Atlantique",
+            "B. Océan Arctique",
+            "C. Océan Pacifique",
+            "D. Océan Indien"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Quel animal est souvent associé à l'Australie ?",
+        answer: "A. Koala",
+        options: [
+            "A. Koala",
+            "B. Panda",
+            "C. Gorille",
+            "D. Tigre"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Quelle est la capitale de la France ?",
+        answer: "A. Paris",
+        options: [
+            "A. Paris",
+            "B. Londres",
+            "C. Berlin",
+            "D. Rome"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Qui a écrit Roméo et Juliette ?",
+        answer: "D. William Shakespeare",
+        options: [
+            "A. F. Scott Fitzgerald",
+            "B. Jane Austen",
+            "C. Charles Dickens",
+            "D. William Shakespeare"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Quel est l'élément chimique représenté par le symbole H dans le tableau périodique ?",
+        answer: "D. Hydrogène",
+        options: [
+            "A. Argon",
+            "B. Fer",
+            "C. Hélium",
+            "D. Hydrogène"
+        ]
+    }
+];
